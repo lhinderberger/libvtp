5132caa8942d4b7134e0678b92a0e95492b59d11
Lucas Hinderberger <git@lhinderberger.com>
2020-11-17 13:29:13 +0100

    Test skeleton for codec

--- tests/codec.c
+++ tests/codec.c
@@ -1,14 +1,39 @@
 #include "../vendor/greatest/greatest.h"
 
-TEST foo(void) {
+TEST increment_time_can_be_decoded(void) {
     FAIL();
 }
 
-TEST bar(void) {
-    PASS();
+TEST increment_time_can_be_encoded(void) {
+    FAIL();
+}
+
+TEST set_amplitude_can_be_decoded(void) {
+    FAIL();
+}
+
+TEST set_amplitude_can_be_encoded(void) {
+    FAIL();
+}
+
+TEST set_frequency_can_be_decoded(void) {
+    FAIL();
+}
+
+TEST set_frequency_can_be_encoded(void) {
+    FAIL();
+}
+
+TEST invalid_instruction_code_yields_error(void) {
+    FAIL();
 }
 
 GREATEST_SUITE(codec_suite) {
-    RUN_TEST(foo);
-    RUN_TEST(bar);
+    RUN_TEST(increment_time_can_be_decoded);
+    RUN_TEST(increment_time_can_be_encoded);
+    RUN_TEST(set_amplitude_can_be_decoded);
+    RUN_TEST(set_amplitude_can_be_encoded);
+    RUN_TEST(set_frequency_can_be_decoded);
+    RUN_TEST(set_frequency_can_be_encoded);
+    RUN_TEST(invalid_instruction_code_yields_error);
 }
